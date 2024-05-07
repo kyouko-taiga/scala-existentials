7f51896bfb6fdf3c7b42039a3b2850e4281cd99e
Matt Bovel <matthieu@bovel.net>
2024-05-07 19:23:05 +0200

    Set `platforms`

--- swift/Package.swift
+++ swift/Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
   name: "Rays",
+  platforms: [
+      .macOS(.v10_15)
+  ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-numerics", from: "1.0.2"),
   ],
