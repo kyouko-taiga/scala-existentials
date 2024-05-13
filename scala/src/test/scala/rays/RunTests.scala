fb58dbb3da724a70fb8396e74393000d14349a7d
Matt Bovel <matthieu@bovel.net>
2024-05-13 20:02:15 +0200

    Add more run test cases

--- scala/src/test/scala/rays/RunTests.scala
+++ scala/src/test/scala/rays/RunTests.scala
@@ -19,7 +19,56 @@ class RandomTests extends munit.FunSuite {
     assertEquals(run(512), 12)
   }
 
+  test("run(525)") {
+    assertEquals(run(525), 13)
+  }
+
+  test("run(550)") {
+    assertEquals(run(550), 14)
+  }
+
+  test("run(575)") {
+    assertEquals(run(575), 15)
+  }
+
+  test("run(587)") {
+    assertEquals(run(587), 15)
+  }
+
+  test("run(592)") {
+    assertEquals(run(592), 15)
+  }
+
+  test("run(596)") {
+    assertEquals(run(596), 15)
+  }
+
+  test("run(597)") {
+    assertEquals(run(597), 16) // failing
+  }
+
+  test("run(598)") {
+    assertEquals(run(598), 16) // failing
+  }
+
+  test("run(600)") {
+    assertEquals(run(600), 16) // failing
+  }
+
+  test("run(700)") {
+    assertEquals(run(700), 21) // failing
+  }
+
   test("run(768)") {
     assertEquals(run(768), 26) // failing
   }
+
+  test("run(1024)") {
+    assertEquals(run(1024), 49) // failing
+  }
+
+  test("run(2048)") {
+    assertEquals(run(2048), 155) // failing
+  }
+
 }
